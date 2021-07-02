b1a95191530c518e8d51f873af1a5a95245aebd2
tamura <tamuten310@gmail.com>
2021-07-02 20:35:58 +0900

    初回コミット

---
+++ INSERT_TEST_DATA.sql
@@ -0,0 +1,6 @@
+INSERT INTO lesson VALUES ('1', 'test', 'test', '50');
+INSERT INTO lesson VALUES ('2', 'test', 'test', '1');
+INSERT INTO lesson VALUES ('00000003', 'パズル道場【図形・思考力】', 'パ図', '50');
+INSERT INTO lesson VALUES ('00000004', 'パズル道場【数】', 'パ数', '50');
+INSERT INTO lesson VALUES ('00000005', '個別演習ゼミ', '演習', '50');
+
